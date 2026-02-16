c03b192bb989ed91483f954e529a3dc5311ccc1e
valecristiano <valeriamdcristiano@gmail.com>
2026-02-16 17:32:42 +0300

    form logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,61 @@
 import axios from "axios";
 import Header from "./components/Header";
+import { useState } from "react";
 
 export default function App() {
+  const [formState, setFormState] = useState({
+    postAuthor: "",
+    postTitle: "",
+    postText: "",
+    isPublic: false,
+  });
+
+  const handleFormChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    setFormState({
+      ...formState,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
   return (
     <>
       <Header></Header>
       <main>
         <section className="container">
           <form className="row g-3 mt-3">
+            {/* //autore */}
             <div className="col-md-6">
-              <label for="inputEmail4" className="form-label">
+              <label htmlFor="author" className="form-label">
                 Autore
               </label>
-              <input type="text" className="form-control" id="inputEmail4" />
+              <input name="postAuthor" value={formState.postAuthor} onChange={handleFormChange} type="text" className="form-control" id="author" />
             </div>
+            {/* titolo */}
             <div className="col-md-6">
-              <label for="inputPassword4" className="form-label">
+              <label htmlFor="title" className="form-label">
                 Titolo
               </label>
-              <input type="text" className="form-control" id="inputPassword4" />
+              <input name="postTitle" value={formState.postTitle} onChange={handleFormChange} type="text" className="form-control" id="title" />
             </div>
+            {/* testo */}
             <div className="col-12">
-              <label for="inputAddress" className="form-label">
+              <label htmlFor="text-area" className="form-label">
                 Testo
               </label>
-
-              <textarea className="form-control" aria-label="With textarea" id="inputAddress"></textarea>
+              <textarea name="postText" value={formState.postText} onChange={handleFormChange} className="form-control" aria-label="With textarea" id="text-area"></textarea>
             </div>
-
+            {/* checkbox */}
             <div className="col-12">
               <div className="form-check">
-                <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault1" />
-                <label className="form-check-label" for="radioDefault1">
-                  Default radio
-                </label>
-              </div>
-              <div className="form-check">
-                <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault2" checked />
-                <label className="form-check-label" for="radioDefault2">
-                  Default checked radio
+                <input name="isPublic" checked={formState.isPublic} onChange={handleFormChange} className="form-check-input" type="checkbox" id="gridCheck" />
+                <label className="form-check-label" htmlFor="gridCheck">
+                  Pubblico
                 </label>
               </div>
             </div>
-
+            {/* bottone */}
             <div className="col-12">
               <button type="submit" className="btn btn-outline-warning px-3">
                 Invia
